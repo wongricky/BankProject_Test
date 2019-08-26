fa5c37219d2ab735ca4a18ea9163da686a2b4f07
Ricky Wong <rickywong@dtirvingconsult.dental.local>
2019-08-26 12:58:43 -0500

    First Commit

---
+++ src/main/java/com/team3/bankApp/MeritBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.team3.bankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritBankingAppApplication.class, args);
+	}
+
+}
